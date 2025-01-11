7201b91b026711a08ca05e283f5baeb0955ef9fb
alberto cossa <alberto.er.cossa@gmail.com>
2025-01-11 13:53:35 +0000

    first commit

---
+++ my-api/api/cadastrar.js
@@ -0,0 +1,27 @@
+const mysql = require('mysql2');
+
+// Configuração da conexão com o MySQL no AlwaysData
+const connection = mysql.createConnection({
+  host: 'mysql-xitique.alwaysdata.net',
+  user: 'xitique',
+  password: 'Acossa@824018...84',
+  database: 'xitique_cash'
+});
+
+module.exports = (req, res) => {
+  if (req.method === 'POST') {
+    // Pega os dados do corpo da requisição
+    const { nome, turma } = req.body;
+    
+    // Insere os dados no banco
+    const query = 'INSERT INTO alunos (nome, turma) VALUES (?, ?)';
+    connection.query(query, [nome, turma], (err, results) => {
+      if (err) {
+        return res.status(500).json({ error: 'Erro ao cadastrar usuário' });
+      }
+      res.status(200).json({ message: 'Usuário cadastrado com sucesso!', id: results.insertId });
+    });
+  } else {
+    res.status(405).json({ error: 'Método não permitido' });
+  }
+};
